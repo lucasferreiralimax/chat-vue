44a51b66bde1410c6aac7d33152906f64d88922c
lucasferreiralimax <lucasferreiralimax@gmail.com>
2020-08-28 20:26:22 -0300

    page 404

--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,19 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/404',
+    name: 'Notfound',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "notfound" */ '../views/Notfound.vue')
+  },
+  {
+    path: '*',
+    redirect: '/404'
+  },
 ]
 
 const router = new VueRouter({
---
+++ src/views/Notfound.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="notfound">
+    <h1>Page 404</h1>
+    <router-link to="/" tag="a">
+      Voltar para o chat
+    </router-link>
+  </div>
+</template>

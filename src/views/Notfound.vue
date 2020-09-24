8621013b95490c20a87fd3c7d98b46cdb7108c3d
lucasferreiralimax <lucasferreiralimax@gmail.com>
2020-09-23 23:53:18 -0300

    styles in page notfound

--- src/views/Notfound.vue
+++ src/views/Notfound.vue
@@ -1,8 +1,21 @@
 <template>
   <div class="notfound">
     <h1>Page 404</h1>
+    <p>Desculpe o inconveniente essa página não existe</p>
     <router-link to="/" tag="a">
-      Voltar para o chat
+      Clique aqui para ir para Home
     </router-link>
   </div>
 </template>
+
+<style lang="stylus">
+  .notfound
+    display flex
+    align-items center
+    justify-content center
+    min-height 100vh
+    flex-direction column
+    a
+      text-decoration none
+      color #2cca6b
+</style>

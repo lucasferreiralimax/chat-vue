32cbcc4e49da84a2f4617028d64cd966237525ce
lucasferreiralimax <lucasferreiralimax@gmail.com>
2020-09-25 23:58:34 -0300

    Replace text about interface with link vuejs

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="about">
-    <h1>Chat Page About Example Cha</h1>
+  <div class="page about">
+    <h1>Interface de chat simples com responsividade criado com <a href="https://vuejs.org">Vue.js</a></h1>
   </div>
 </template>
+
+<style lang="stylus">
+  .about
+    display flex
+    align-items center
+    justify-content center
+    min-height 100vh
+    flex-direction column    
+</style>

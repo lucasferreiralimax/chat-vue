4a4a6b79647c5aaef5cceef9f1d7fc44acaedc05
lucasferreiralimax <lucasferreiralimax@gmail.com>
2020-10-16 20:14:21 -0300

    chat current find in Home component with redirect Notfound

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,7 +15,28 @@ import Chat from '@/components/chat/Chat'
 
 export default {
   name: 'Home',
-  computed: mapState(['chats', 'chats_list']),
+  computed: mapState(['chats_list']),
+  created () {
+    let chat_current = this.chats_list.find(el => el.url == this.$route.params.id)
+    if(chat_current) {
+      this.chats = chat_current.chats
+    } else {
+      this.$router.push({ name: 'Notfound' })
+    }
+  },
+  watch: {
+    $route(to, from) {
+      console.log(to)
+      console.log(from)
+      let chat_current = this.chats_list.find(el => el.url == this.$route.params.id)
+      console.log(chat_current)
+      if(chat_current) {
+        this.chats = chat_current.chats
+      } else {
+        this.$router.push({ name: 'Notfound' })
+      }
+    }
+  },
   components: {
     ChatHeader,
     ChatList,
@@ -28,7 +49,8 @@ export default {
   },
   data () {
     return {
-      menu: ''
+      menu: '',
+      chats: []
     }
   }
 }

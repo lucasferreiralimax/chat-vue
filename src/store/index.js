15f9e32b0e41f64db66ee5532ae618c4ae169f1c
lucasferreiralimax <lucasferreiralimax@gmail.com>
2020-11-26 01:32:17 -0300

    example user in store vuex

--- src/store/index.js
+++ src/store/index.js
@@ -6,6 +6,13 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    user: {
+      name: 'L',
+      status: 'online',
+      status_persona: 'Meus versos vivos te farão viver.',
+      photo: 'https://avatars2.githubusercontent.com/u/40927839?s=460&u=25362ddd9f12b82fc4484fd8298e29c8564ab0d7',
+      url: 'lucas',
+    },
     chats_list: chats_list
   },
   mutations: {

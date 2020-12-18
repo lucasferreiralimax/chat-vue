5e5d229f011dfb295eeaab78ef94244702f43cfb
lucasferreiralimax <lucasferreiralimax@gmail.com>
2020-12-18 15:41:05 -0800

    new routes project

--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,21 @@ const routes = [
         component: () => import(/* webpackChunkName: "welcome" */ '../views/Welcome.vue')
       },
       {
+        path: '/profile',
+        name: 'profile-config',
+        component: () => import(/* webpackChunkName: "profile" */ '../views/Profile.vue')
+      },
+      {
+        path: '/profile/:id',
+        name: 'profile',
+        component: () => import(/* webpackChunkName: "profile" */ '../views/Profile.vue')
+      },
+      {
+        path: '/config',
+        name: 'config',
+        component: () => import(/* webpackChunkName: "config" */ '../views/Config.vue')
+      },
+      {
         path: 'chat/:id',
         name: 'chat',
         component: () => import(/* webpackChunkName: "chat" */ '../views/Chat.vue')

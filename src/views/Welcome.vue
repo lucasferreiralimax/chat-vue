dc8615f38efd7d4fba69bd5e2a8a5e6fec695508
lucasferreiralimax <lucasferreiralimax@gmail.com>
2020-12-21 12:43:27 -0800

    adjustment style welcome home

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,17 +1,24 @@
 <template>
-    <section class="chat-content">
-        <h1>Seja bem-vindo {{ user }}</h1>
-        <p>Uma interface simples de chat criada com Vue.js</p>
-    </section>    
+  <section class="chat-content welcome">
+    <h1>Seja bem-vindo {{ user }}</h1>
+    <p>Uma interface simples de chat criada com Vue.js</p>
+  </section>    
 </template>
 
 <script>
 export default {
   name: 'Welcome',
   data () {
-      return {
-          user: 'Lucas Lima'
-      }
+    return {
+      user: 'Lucas Lima'
+    }
   }
 }
 </script>
+
+<style lang="stylus">
+@media screen and (max-width 830px)
+  .welcome
+    padding 0 1.5em
+    height auto !important
+</style>

ddf36a2fc9f2ecb68dc2808855ff80eb0b06bdec
lucasferreiralimax <lucasferreiralimax@gmail.com>
2020-12-22 14:05:29 -0800

    jest test unit with window matchMedia

--- tests/unit/jest.config.js
+++ tests/unit/jest.config.js
@@ -1,3 +1,13 @@
 import { config } from '@vue/test-utils';
 
 config.mocks.$t = key => key;
+
+Object.defineProperty(window, 'matchMedia', {
+  value: () => {
+    return {
+      matches: false,
+      addListener: () => {},
+      removeListener: () => {}
+    };
+  }
+});

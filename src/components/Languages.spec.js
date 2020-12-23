9ce21953ba27de764b88e6e89e8e8162c0e54955
lucasferreiralimax <lucasferreiralimax@gmail.com>
2020-12-22 16:42:46 -0800

    test:unit language spec basic with i18n localVue

---
+++ src/components/Languages.spec.js
@@ -0,0 +1,34 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import VueI18n from 'vue-i18n'
+
+import Languages from './Languages.vue'
+
+const localVue = createLocalVue()
+localVue.use(VueI18n)
+
+function loadLocaleMessages () {
+  const locales = require.context('./locales', true, /[A-Za-z0-9-_,\s]+\.json$/i)
+  const messages = {}
+  locales.keys().forEach(key => {
+    const matched = key.match(/([A-Za-z0-9-_]+)\./i)
+    if (matched && matched.length > 1) {
+      const locale = matched[1]
+      messages[locale] = locales(key)
+    }
+  })
+  return messages
+}
+
+const i18n = new VueI18n({
+  locale: 'en',
+  fallbackLocale: 'en',
+  messages: loadLocaleMessages()
+})
+
+const wrapper = shallowMount(Languages, { i18n, localVue })
+
+describe('Languages', () => {
+  it('is Languages a vue instance', () => {
+    expect(wrapper.isVueInstance()).toBe(true)
+  })
+})

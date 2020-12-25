281721233fcc9fce13d053a066a282305183a0bf
lucasferreiralimax <lucasferreiralimax@gmail.com>
2020-12-24 18:54:03 -0800

    component language in config page

--- src/views/Config.vue
+++ src/views/Config.vue
@@ -3,14 +3,16 @@
     <h1>{{ this.$t("views.config.title") }}</h1>
     <p>{{ this.$t("views.config.text") }}</p>
     <dark-mode />
+    <div>{{ this.$t("views.config.language") }}<languages /></div>
   </section>    
 </template>
 
 <script>
 import DarkMode from '@/components/DarkMode'
+import Languages from '@/components/Languages'
 
 export default {
   name: 'Config',
-  components: { DarkMode }
+  components: { DarkMode, Languages }
 }
 </script>

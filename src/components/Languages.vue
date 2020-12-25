48692c4164526b93b167a41ff88e40c834ff7e8a
lucasferreiralimax <lucasferreiralimax@gmail.com>
2020-12-25 05:23:57 -0800

    update lang code, adjustment html lang and localStorage lang :hammer:

--- src/components/Languages.vue
+++ src/components/Languages.vue
@@ -1,6 +1,6 @@
 <template lang="html">
   <select class="languages" v-model="$i18n.locale">
-    <option v-for="(lang, i) in langs" :key="`Lang${i}`" :value="lang">{{ lang }}</option>
+    <option v-for="(lang, i) in langs" :key="`Lang-${i}`" :value="lang">{{ lang }}</option>
   </select>
 </template>
 
@@ -9,7 +9,20 @@ export default {
   name: 'languages',
   data () {
     return {
-      langs: ['pt', 'en', 'ru', 'es', 'fr']
+      langs: ['pt-BR', 'en-US', 'ru-RU', 'es-ES', 'fr-FR']
+    }
+  },
+  created () {
+    let localLang = localStorage.getItem("lang") ? localStorage.getItem("lang") : "en-US"
+    if(localLang) {
+      this.$i18n.locale = localLang
+      document.querySelector('html').setAttribute('lang', localLang)
+    }
+  },
+  watch: {
+    '$i18n.locale' (value) {
+      localStorage.setItem("lang", value)
+      document.querySelector('html').setAttribute('lang', value)
     }
   }
 }
@@ -17,6 +30,7 @@ export default {
 
 <style lang="stylus">
 .languages
+  border-radius 5px
   margin 1em
   padding .5em
 </style>
